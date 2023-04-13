fd0d6329e4eeb5751f2556f40d51e9009cdb12f0
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-04-13 16:04:15 +0300

    translate into English

--- sicp/chapter1/1_4.rkt
+++ sicp/chapter1/1_4.rkt
@@ -2,11 +2,13 @@
 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
-;По правилу вычисления комбинации, определенному в 1.1.3,
-;сначала необходимо вычислить значение всех подвыражений.
-;Для того, чтобы вычислить значение оператора комбинации
-;в теле функции, нам нужно получить значение предиката
-;(> b 0). Если вернется значение "true", то оператором  
-;комбинации будет "+", в противном случае оператором 
-;будет "-". Таким образом, комбинация, определенная в теле
-;функции, будет редуцирована до (+ a b) или (- a b).
+
+; According to the combination evaluation rule (1.1.3)
+; value of all subexpressions will be calculated first.
+; In order to calculate the value of the operator
+; in the function body, we need to get the value
+; of the predicate (> b 0). If its return "true",
+; then operator will be "+", otherwise the operator
+; will be "-". Thus, combination difined in the body
+; of function, when substituting arguments, will be
+; reduce to (+ a b) or (- a b).
